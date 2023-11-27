44666dd36976bbb09cf33a45a515039778dab760
Christopher Banta <chris.banta@rocketmail.com>
2023-11-26 23:58:50 -0600

    Added base layout and files for server folder

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.stk0aow.mongodb.net/lazydogCustomerReviews`
+);
+
+module.exports = mongoose.connection;
